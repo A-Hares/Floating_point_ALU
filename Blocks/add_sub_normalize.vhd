7ee463c5d9a3b4cf46e736050094354f32a6d295
Peter Gad <139645814+petergad14@users.noreply.github.com>
2023-08-03 13:32:07 +0300

    Update add_sub_normalize.vhd

--- Blocks/add_sub_normalize.vhd
+++ Blocks/add_sub_normalize.vhd
@@ -11,6 +11,7 @@ entity addmant is
     A_sign: in std_logic;
     B_sign: in std_logic;
     E_pre: in STD_LOGIC_VECTOR(7 downto 0);
+    en: in std_logic;
     fract: out STD_LOGIC_VECTOR(22 downto 0);
     sum_sign: out std_logic;
     exp: out STD_LOGIC_VECTOR(7 downto 0);
@@ -19,57 +20,73 @@ end;
 
 architecture synth of addmant is
     type state_type is (add_state, normalize_state, done_state);
-    signal state : state_type;
+    signal state, next_state : state_type;
     signal M_sum: STD_LOGIC_VECTOR(24 downto 0);
     signal exp_temp: std_logic_vector (7 downto 0);
 
 begin
-    DONE <= '1' when state = done_state else
-            '0';
-    fract <= M_sum(22 downto 0);
-    exp <= exp_temp;
-    process(clk,rst,state,M_A_Shifted,M_B_Shifted,exp_temp,M_sum)
+    process (clk, rst, en, next_state)
     begin
         if rst = '0' then
             state <= add_state;
         elsif rising_edge(clk) then
-            case state is
-                when add_state =>
-                    exp_temp <= E_pre;
-                    if (A_sign xor B_sign) = '0' then --same sign
-                        M_sum <= std_logic_vector(unsigned(M_A_Shifted) + unsigned(M_B_Shifted));  
-                        sum_sign <= A_sign;
-                        state <= normalize_state;
+            if (en = '1') then
+                state <= next_state;
+            end if;
+        end if;        
+    end process;
 
-                    elsif (M_A_Shifted >= M_B_Shifted) then --Mantessa A bigger than Mantessa B
-                        M_sum <= std_logic_vector(unsigned(M_A_Shifted) - unsigned(M_B_Shifted));
-                        sum_sign <= A_sign;
-                        state <= normalize_state;
 
-                    else
-                        M_sum <= std_logic_vector(unsigned(M_B_Shifted) - unsigned(M_A_Shifted));
-                        sum_sign <= B_sign;
-                        state <= normalize_state;
-                    end if;
+    
+    process(state,M_A_Shifted,M_B_Shifted, M_sum)
+    begin
+        next_state <= state;
+        DONE <= '0';
+        case state is
+            when add_state =>
+                exp_temp <= E_pre;
+                if (A_sign xor B_sign) = '0' then --same sign
+                    M_sum <= std_logic_vector(unsigned(M_A_Shifted) + unsigned(M_B_Shifted));  
+                    sum_sign <= A_sign;
+                    next_state <= normalize_state;
+
+                elsif (M_A_Shifted > M_B_Shifted) then --Mantessa A bigger than Mantessa B
+                    M_sum <= std_logic_vector(unsigned(M_A_Shifted) - unsigned(M_B_Shifted));
+                    sum_sign <= A_sign;
+                    next_state <= normalize_state;
                 
-                when normalize_state =>
-                    if (M_sum(24) = '1') then
-                        M_sum <= '0' & M_sum(24 downto 1);
-                        exp_temp <= std_logic_vector(unsigned(exp_temp) + 1);
-                        state <= normalize_state;
-                    elsif (M_sum(23) = '0') then
-                        M_sum <= M_sum(23 downto 0) & '0';
-                        exp_temp <= std_logic_vector(unsigned(exp_temp) - 1);
-                        state <= normalize_state;
-                    else
-                        state <= done_state;
-                    end if;
+                elsif (M_A_Shifted < M_B_Shifted) then --Mantessa B bigger than Mantessa A
+                    M_sum <= std_logic_vector(unsigned(M_B_Shifted) - unsigned(M_A_Shifted));
+                    sum_sign <= B_sign;
+                    next_state <= normalize_state;
 
-                when done_state => 
-                    state <= add_state;   
-                when others => 
-                    state <= add_state;     
-            end case;
-        end if;
+                else --Mantessas are equal
+                    M_sum <= std_logic_vector(unsigned(M_B_Shifted) - unsigned(M_A_Shifted));
+                    sum_sign <= '0';
+                    next_state <= done_state;
+                    exp_temp <= "00000000";
+                end if;
+            
+            when normalize_state =>
+                if (M_sum(24) = '1') then
+                    M_sum <= '0' & M_sum(24 downto 1);
+                    exp_temp <= std_logic_vector(unsigned(exp_temp) + 1);
+                    next_state <= normalize_state;
+                elsif (M_sum(23) = '0') then
+                    M_sum <= M_sum(23 downto 0) & '0';
+                    exp_temp <= std_logic_vector(unsigned(exp_temp) - 1);
+                    next_state <= normalize_state;
+                else
+                next_state <= done_state;
+                end if;
+
+            when done_state =>
+            DONE <= '1';
+            fract <= M_sum(22 downto 0);
+            exp <= exp_temp;
+            next_state <= add_state;
+            when others => 
+            next_state <= add_state;     
+        end case;
     end process;
 end;
